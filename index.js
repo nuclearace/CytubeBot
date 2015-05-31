84f4fba8c36bd8716937f3eed12df7a713d417a7
Erik <nuclear.ace@gmail.com>
2015-05-31 09:40:04 -0400

    Atom beautify

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 var forever = require("forever-monitor")
 var fs = require("fs")
 
-var child = new (forever.Monitor)("./lib/start.js", {
-	max: 21,
-	silent: false,
-	minUptime: 5000,
-	errFile: "./err.log"
+var child = new(forever.Monitor)("./lib/start.js", {
+    max: 21,
+    silent: false,
+    minUptime: 5000,
+    errFile: "./err.log"
 })
 
 var writeTimes = function() {
-	fs.writeFile("times", String(child.times), function(err) {
-		if (err) {
-			console.log(err)
-			child.stop()
-			process.exit(1)
-		}
-	})
+    fs.writeFile("times", String(child.times), function(err) {
+        if (err) {
+            console.log(err)
+            child.stop()
+            process.exit(1)
+        }
+    })
 }
 
 child.on("exit", function() {
-	console.log("$~~~$ CytubeBot has exited after 20 restarts or there was a problem\n")
-	console.log("$~~~$ Shutting down")
+    console.log("$~~~$ CytubeBot has exited after 20 restarts or there was a problem\n")
+    console.log("$~~~$ Shutting down")
 
 })
 
 child.on("restart", function() {
-	console.log("$~~~$ CytubeBot is restarting after a close\n")
-	writeTimes()
+    console.log("$~~~$ CytubeBot is restarting after a close\n")
+    writeTimes()
 })
 
 child.start()
--- www/js/emotes.js
+++ www/js/emotes.js
@@ -1,43 +1,43 @@
 socket = io(IO_URL)
 setTimeout(function() {
-	socket.emit("getRoom")
-	socket.emit("getEmotes")
+    socket.emit("getRoom")
+    socket.emit("getEmotes")
 }, 1000)
 
 var addEmote = function(emote) {
-	var name = emote["name"]
-	var image = emote["image"]
-	var tbl = $("#emotediv table")
-	var tr = $("<tr/>").appendTo(tbl)
+    var name = emote["name"]
+    var image = emote["image"]
+    var tbl = $("#emotediv table")
+    var tr = $("<tr/>").appendTo(tbl)
 
-	var emoteDiv = $("<span>").text(name)
-		.appendTo($("<td/>").appendTo(tr))
+    var emoteDiv = $("<span>").text(name)
+        .appendTo($("<td/>").appendTo(tr))
 
-	var popoverData = {
-		html: true,
-		trigger: "hover",
-		content: '<img src="' + image + '" class="channel-emote">'
-	}
-	emoteDiv.popover(popoverData)
+    var popoverData = {
+        html: true,
+        trigger: "hover",
+        content: '<img src="' + image + '" class="channel-emote">'
+    }
+    emoteDiv.popover(popoverData)
 }
 
 var handleEmotes = function(emotes) {
-	emotes.sort(function(a, b) {
-		if (a["name"] > b["name"])
-			return 1
-		if (a["name"] < b["name"])
-			return -1
-		if (a["name"] = b["name"])
-			return 0
-	})
-	emotes.forEach(function(emote) {
-		addEmote(emote)
-	})
-	socket.disconnect()
+    emotes.sort(function(a, b) {
+        if (a["name"] > b["name"])
+            return 1
+        if (a["name"] < b["name"])
+            return -1
+        if (a["name"] = b["name"])
+            return 0
+    })
+    emotes.forEach(function(emote) {
+        addEmote(emote)
+    })
+    socket.disconnect()
 }
 
 var handleRoom = function(room) {
-	$("h1").text(room + " Emotes")
+    $("h1").text(room + " Emotes")
 }
 
 socket.on("emotes", handleEmotes)
