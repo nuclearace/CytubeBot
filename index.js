866bdeadd37b35a8afefa516af64c2946c4e29b3
airforce270 <96109779+airforce270@users.noreply.github.com>
2022-02-13 22:27:00 -0800

    Improve reliability for crashes.
    Will now restart after unhandled exceptions.

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 import {Monitor} from 'forever-monitor';
-import {writeFile} from 'fs/promises';
+import {readFile, writeFile} from 'fs/promises';
 
 import {errlog} from './lib/logger.js';
 
-const MAX_RESTARTS = 20;
+const MAX_RESTARTS = 30;
 
 const child = new Monitor('./lib/start.js', {
   max: MAX_RESTARTS + 1,
   silent: false,
   minUptime: 5000,
-  errFile: './err.log',
+  errFile: './monitor-error.log',
 });
 
 /** Write the number of times the bot has been restarted to the file. */
@@ -24,17 +24,27 @@ async function writeTimes() {
   }
 };
 
-child.on('exit', () => {
+child.on('exit', async () => {
+  const timesBuffer = await readFile('times');
+  const times = parseInt(timesBuffer.toString(), 10);
+  if (isNaN(times)) {
+    console.log('$~~~$ Failed to read restart count (isNaN)\n');
+  }
+
+  // If the exit code !== 0, forever-monitor won't restart it.
+  // So do it manually.
+  if (times < MAX_RESTARTS) {
+    console.log('$~~~$ CytubeBot is restarting\n');
+    child.start(/** restart= */ true);
+    writeTimes();
+    return;
+  }
+
   console.log(
       `$~~~$ CytubeBot has exited after ${MAX_RESTARTS} restarts ` +
       'or there was a problem\n');
   console.log('$~~~$ Shutting down');
 });
 
-child.on('restart', () => {
-  console.log('$~~~$ CytubeBot is restarting after a close\n');
-  writeTimes();
-});
-
-child.start();
+child.start(/** restart= */ false);
 writeTimes();
