33f5d1f3ad702cadb8e5fa65365cd9dc4be5d83d
airforce270 <96109779+airforce270@users.noreply.github.com>
2022-02-13 22:55:38 -0800

    Extract constants, rename files

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
 import {Monitor} from 'forever-monitor';
 import {readFile, writeFile} from 'fs/promises';
 
-import {errlog} from './lib/logger.js';
-
-const MAX_RESTARTS = 30;
+import {MAX_RESTARTS, MONITOR_ERROR_LOG_FILE_NAME, RESTART_TIMES_FILE_NAME} from './lib/constants.js';
+import {errorLog} from './lib/logger.js';
 
 const child = new Monitor('./lib/start.js', {
   max: MAX_RESTARTS + 1,
   silent: false,
   minUptime: 5000,
-  errFile: './monitor-error.log',
+  errFile: MONITOR_ERROR_LOG_FILE_NAME,
 });
 
 /** Write the number of times the bot has been restarted to the file. */
 async function writeTimes() {
   try {
-    await writeFile('times', String(child.times));
+    await writeFile(RESTART_TIMES_FILE_NAME, String(child.times));
   } catch (err) {
-    errlog.log(err);
+    errorLog.log(err);
     console.log(err);
     child.stop();
     process.exit(1);
@@ -25,7 +24,7 @@ async function writeTimes() {
 };
 
 child.on('exit', async () => {
-  const timesBuffer = await readFile('times');
+  const timesBuffer = await readFile(RESTART_TIMES_FILE_NAME);
   const times = parseInt(timesBuffer.toString(), 10);
   if (isNaN(times)) {
     console.log('$~~~$ Failed to read restart count (isNaN)\n');
